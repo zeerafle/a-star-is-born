787b83f8a09bdd1d503f8b65f251fd8633a92fb8
zeerafle <vsefareez@gmail.com>
2021-11-26 13:43:39 +0800

    implemented showing selected maze with its numbers

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@ tosca gelap : 0081a7
 """
 from pathlib import Path
 import tkinter as tk
+import numpy as np
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path("./assets")
 
@@ -118,7 +119,7 @@ class MazeSelection:
             image=self.button_image_1,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: self.go_to_selected_maze(1),
+            command=lambda: self.go_to_selected_maze(5),
             relief="flat"
         )
         self.button_1.place(
@@ -134,7 +135,7 @@ class MazeSelection:
             image=self.button_image_2,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: self.go_to_selected_maze(2),
+            command=lambda: self.go_to_selected_maze(8),
             relief="flat"
         )
         self.button_2.place(
@@ -150,7 +151,7 @@ class MazeSelection:
             image=self.button_image_3,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: self.go_to_selected_maze(3),
+            command=lambda: self.go_to_selected_maze(10),
             relief="flat"
         )
         self.button_3.place(
@@ -166,7 +167,7 @@ class MazeSelection:
             image=self.button_image_4,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: self.go_to_selected_maze(4),
+            command=lambda: self.go_to_selected_maze(12),
             relief="flat"
         )
         self.button_4.place(
@@ -176,23 +177,23 @@ class MazeSelection:
             height=266.0
         )
 
-    def go_to_selected_maze(self, maze_number):
+    def go_to_selected_maze(self, maze_size):
         """
         Fungsi untuk berpindah ke
         frame maze yang dipilih untuk
         selanjutnya memilih titik awal dan akhir
         """
         self.frame.forget()
-        self.app = MazeFrame(self.master, maze_number)
+        self.app = MazeFrame(self.master, maze_size)
 
 class MazeFrame:
     """
     Frame untuk menu labirin
     Yang selanjutnya pilih awal dan akhir
     """
-    def __init__(self, master, maze_number):
+    def __init__(self, master, maze_size):
         self.master = master
-        self.maze_number = maze_number
+        self.maze_size = maze_size
         self.frame = tk.Frame(self.master)
 
         self.canvas = tk.Canvas(
@@ -277,7 +278,75 @@ class MazeFrame:
             font=("IBMPlexSans SemiBold", 14 * -1)
         )
 
+        self.sisi = 290/self.maze_size
         self.frame.pack(fill="both", expand=1)
+        self.maze_to_show(self.maze_size)
+        self.maze_matrix = self.create_maze_matrix(self.maze_size)
+        self.place_numbers(self.maze_matrix, self.maze_size, self.sisi)
+
+    def maze_to_show(self, maze_size):
+        """
+        Menampilkan labirin yang dipilih
+        berdasarkan maze_size yang dimasukkan
+        """
+        self.file_name = "maze" + str(maze_size) + "_pure.png"
+        self.image_file = tk.PhotoImage(file=relative_to_assets(self.file_name))
+
+        # bikin semacam container untuk labirinnya
+        self.canvas_maze = tk.Canvas(
+            self.frame,
+            height=290,
+            width=290,
+            relief="flat"
+        )
+        self.canvas_maze.place(x=394.0, y=164.0,)
+        self.canvas_maze.create_image(
+            145.5,
+            145.5,
+            image=self.image_file
+        )
+
+    def create_maze_matrix(self, maze_size):
+        """
+        Membuat matrix yang sesuai dengan maze_size
+        Ukuran labirin dalam frame ini adalah 290x290.
+        Maka, panjang sisi satu petak = 290/maze_size.
+        """
+        maze_matrix = np.arange(1, maze_size*maze_size+1).reshape(maze_size, -1)
+        return np.transpose(maze_matrix)
+
+    def place_numbers(self, maze_matrix, maze_size, sisi):
+        """
+        Menempatkan angka di tiap petak
+        """
+        x_1 = y_1 = sisi/2
+        for column in range(maze_size):
+            for row in range(maze_size):
+                self.canvas_maze.create_text(
+                    x_1,
+                    y_1,
+                    text=str(maze_matrix[row,column]),
+                    font=("Arial", 7)
+                )
+                y_1 += sisi # karena ini iterasi baris maka y yang bergerak
+            x_1 += sisi # setelah kolom pertama selesai, x bergerak
+            y_1 = sisi/2 # reset nilai y_1
+
+class ShowSolution:
+    def __init__(self, master):
+        pass
+        # buat petaknya
+        # for node in solusi:
+        #     node_location = np.where(maze_matrix == node)
+        #     x_1 = np.where(transposed == node)[0][0]
+        #     y_1 = np.where(transposed == node)[1][0]
+
+        # self.canvas_maze.create_rectangle(
+        #     0.5,0.5,panjang_sisi,panjang_sisi,
+        #     fill="#fed9b7",
+        #     outline=""
+        # )
+        # solusi = ['7', '15', '23', '31', '30', '29', '21', '22', '14', '13', '5', '4', '12', '20', '28', '27', '19', '11', '10', '2', '1', '9', '17', '18', '26', '34', '35', '36', '44', '52', '60', '61']
 
 def main():
     """
